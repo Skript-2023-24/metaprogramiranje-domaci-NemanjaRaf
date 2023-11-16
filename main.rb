5703d29a51739eee8bba024d32ef8ea5611d5ed2
nmarjanov6121rn <115634775+NemanjaRaf@users.noreply.github.com>
2023-11-16 14:44:42 +0100

    Add files via upload

--- main.rb
+++ main.rb
@@ -24,6 +24,15 @@ class Column
   def avg
     @values.empty? ? 0 : @values.sum.to_f / @values.size
   end
+
+  def method_missing(method_name, *args, &block)
+    lookup_value = method_name.to_s
+    table.lookup_row_by_column_value(column_name, lookup_value)
+  end
+
+  def respond_to_missing?(method_name, include_private = false)
+    true
+  end
 end
 
 class Table
@@ -97,12 +106,43 @@ class Table
     method_name.to_s.end_with?('Kolona', 'sum', 'avg') || super
   end
 
-  def -(other_table)
-    if headers_match?(other_table)
-      new_data = @data.reject { |row| other_table.data.include?(row) }
+  def +(other_table)
+    if headers_match?(other_table) && same_dimensions?(other_table)
+      new_data = [@data.first]
+
+      @data[1..-1].zip(other_table.data[1..-1]).each do |row1, row2|
+        new_row = row1.zip(row2).map do |cell1, cell2|
+          if numeric?(cell1) && numeric?(cell2)
+            cell1.to_f + cell2.to_f
+          else
+            cell1
+          end
+        end
+        new_data << new_row
+      end
       Table.new(new_data)
     else
-      raise "Headers do not match!"
+      raise "Tables do not have matching headers or dimensions!"
+    end
+  end
+
+  def -(other_table)
+    if headers_match?(other_table) && same_dimensions?(other_table)
+      new_data = [@data.first]
+
+      @data[1..-1].zip(other_table.data[1..-1]).each do |row1, row2|
+        new_row = row1.zip(row2).map do |cell1, cell2|
+          if numeric?(cell1) && numeric?(cell2)
+            cell1.to_f - cell2.to_f
+          else
+            cell1
+          end
+        end
+        new_data << new_row
+      end
+      Table.new(new_data)
+    else
+      raise "Tables do not have matching headers or dimensions!"
     end
   end
 
@@ -115,16 +155,27 @@ class Table
   def headers_match?(other_table)
     @data.first == other_table.data.first
   end
+
+  def same_dimensions?(other_table)
+    @data.length == other_table.data.length && @data.first.length == other_table.data.first.length
+  end
+
+  def numeric?(value)
+    value.to_s.match?(/^\d+(\.\d+)?$/)
+  end
 end
 
 data = (1..ws.num_rows).map { |row| (1..ws.num_cols).map { |col| ws[row, col] } }
 t = Table.new(data)
+t2 = Table.new(data)
 
 puts "Dvodimenzionalni niz: #{t.row(1)}"
 puts "Enumerable svih celija: #{t.map { |cell| cell }}"
 puts "Pristup koloni preko []: #{t['DrugaKolona']}"
-# t['PrvaKolona', 1] = 2556
+t['PrvaKolona', 1] = 2556
 puts "Enumerable svih celija: #{t.map { |cell| cell }}"
-puts "Direktni pristup metodama kolona i izračunavanje sume i proseka: #{t.drugaKolona.sum}, #{t.prvaKolona.avg}"
+puts "Suma i preosek: #{t.drugaKolona.sum}, #{t.prvaKolona.avg}"
 puts "Test: #{t.prvaKolona.map { |cell| cell+1 }}"
+t3 = t + t2
+puts "Razlika dve tabele: #{t3.map { |cell| cell }}"
 
